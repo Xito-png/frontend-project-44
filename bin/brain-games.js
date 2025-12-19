500193f5a6ee7be2118c03940401f822f9affbaa
Арсений Емельянов <senya.emelyanov1gmail.com>
2025-12-19 21:11:18 +0400

    .

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,1 +1,16 @@
-#!/usr/bin/env node
+import readlineSync from 'readline-sync';
+
+const greetUser = () => {
+  console.log('brain-games');
+  console.log('Welcome to the Brain Games!');
+  
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}!`);
+};
+
+export default greetUser;
+
+import greetUser from '../src/cli.js';
+
+greetUser();
+
