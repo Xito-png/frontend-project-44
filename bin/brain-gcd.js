102ae43b768cbc8c88e679734563daa90346e4fd
Арсений Емельянов <senya.emelyanov1gmail.com>
2026-03-23 21:47:08 +0400

    ///

---
+++ bin/brain-gcd.js
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+import { askName } from '../src/cli.js';
+import { runGame } from '../src/index.js';
+import gcdGame from '../src/games/gcd.js';
+
+const name = askName();
+runGame(gcdGame, name);
--- src/games/gcd.js
+++ src/games/gcd.js
@@ -1,10 +1,19 @@
-const gcd = (a, b) => (b === 0 ? a : gcd(b, a % b));
+const getGCD = (a, b) => {
+  while (b !== 0) {
+    const temp = b;
+    b = a % b;
+    a = temp;
+  }
+  return a;
+};
 
 const getQuestionAndAnswer = () => {
   const num1 = Math.floor(Math.random() * 100) + 1;
   const num2 = Math.floor(Math.random() * 100) + 1;
+
   const question = `${num1} ${num2}`;
-  const answer = gcd(num1, num2).toString();
+  const answer = String(getGCD(num1, num2));
+
   return { question, answer };
 };
 
