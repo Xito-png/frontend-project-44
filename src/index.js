cb47d89ed5187d75f5fdf3fc989b46df59651ff8
Арсений Емельянов <senya.emelyanov1gmail.com>
2026-03-23 21:35:04 +0400

    ...

--- src/index.js
+++ src/index.js
@@ -1,21 +1,24 @@
+import readlineSync from 'readline-sync';
+
 export const runGame = (game, name) => {
-    console.log(game.description);
-    const roundsCount = 3;
-  
-    for (let i = 0; i < roundsCount; i += 1) {
-      const { question, answer } = game.getQuestionAndAnswer();
-      console.log(`Question: ${question}`);
-      const userAnswer = readlineSync.question('Your answer: ');
-  
-      if (userAnswer !== answer) {
-        console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${answer}'.`);
-        console.log(`Let's try again, ${name}!`);
-        return;
-      }
-  
-      console.log('Correct!');
+  console.log(game.description);
+  const roundsCount = 3;
+
+  for (let i = 0; i < roundsCount; i += 1) {
+    const { question, answer } = game.getQuestionAndAnswer();
+    console.log(`Question: ${question}`);
+    const userAnswer = readlineSync.question('Your answer: ');
+
+    if (userAnswer !== answer) {
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${answer}'.`);
+      console.log(`Let's try again, ${name}!`);
+      return;
     }
-  
-    console.log(`Congratulations, ${name}!`);
-  };
+
+    console.log('Correct!');
+  }
+
+  console.log(`Congratulations, ${name}!`);
+};
+
   
